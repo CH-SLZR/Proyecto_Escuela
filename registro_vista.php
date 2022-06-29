64e49cdd2b6fc9453b50aa7aeeb05bacc6953a91
Chino94 <chinosalazar94@gmail.com>
2022-06-28 20:13:05 -0500

    commit

--- registro_vista.php
+++ registro_vista.php
@@ -16,7 +16,7 @@
             <h4>Registro de Usuarios</h4>
         </div>
 
-        <form action="registro_sql" method="post">
+        <form action="registro_sql.php" method="post">
             <div class="login-form">
 
                 <h5>Nombre:</h5>
