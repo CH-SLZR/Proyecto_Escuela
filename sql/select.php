97e08fe2f10aa916eb46e8a36751b56b430880af
Chino94 <chinosalazar94@gmail.com>
2022-07-07 12:12:14 -0500

    commit

--- sql/select.php
+++ sql/select.php
@@ -2,11 +2,11 @@
 include('conexion.php');
 
 $select_admin="SELECT * FROM administradores";
-$query=mysqli_query($conn,$select_admin);
+$query_admin=mysqli_query($conn,$select_admin);
 
 $select_paci="SELECT * FROM pacientes";
-$query=mysqli_query($conn,$select_paci);
+$query_paci=mysqli_query($conn,$select_paci);
 
 $select_ini="SELECT * FROM inicio";
-$query=mysqli_query($conn,$select_ini);
+$query_ini=mysqli_query($conn,$select_ini);
 ?>
