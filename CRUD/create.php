54acd8c7ba6e2ad2db89d7b50be00f989643b234
Chino94 <chinosalazar94@gmail.com>
2022-07-07 12:59:46 -0500

    commit

--- CRUD/create.php
+++ CRUD/create.php
@@ -1,6 +1,6 @@
 <?php 
     include("../sql/conexion.php");
-    include("read");
+    include("read.php");
 ?>
 
 <!DOCTYPE html>
@@ -80,7 +80,7 @@
                             <th><?php  echo $row['dni']?></th>
                             <th><?php  echo $row['nombres']?></th>
                             <th><?php  echo $row['apellidos']?></th>
-                            <th><a href="actualizar.php?id=<?php echo $row['cod_estudiante'] ?>"
+                            <th><a href="edit.php?id=<?php echo $row['cod_estudiante'] ?>"
                                     class="btn btn-info">Editar</a></th>
                             <th><a href="delete.php?id=<?php echo $row['cod_estudiante'] ?>"
                                     class="btn btn-danger">Eliminar</a></th>
