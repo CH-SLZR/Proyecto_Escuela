21a844e6b91a3b5a050e965425e735c3ccc84d93
Chino94 <chinosalazar94@gmail.com>
2022-07-07 16:41:44 -0500

    commit

--- CRUD/update.php
+++ CRUD/update.php
@@ -2,13 +2,14 @@
     include("../sql/conexion.php");
     include("read.php");
 
+$id = $_POST['id'];
 $name = $_POST["name"];
-$ape_p = $_POST["ape_pat"];
-$ape_m = $_POST["ape_mat"];
+$ape_pat = $_POST["ape_pat"];
+$ape_mat = $_POST["ape_mat"];
 $email = $_POST["email"];
 $tel = $_POST["tel"];
 
-$sql="UPDATE administradores SET nombre='$name', apellido_paterno='$ape_pat', apellido_mateno='$ape_mat', email='$email', telefono='$tel' WHERE nombre='$name'";
+$sql="UPDATE administradores SET id='$id', nombre='$name', apellido_paterno='$ape_pat', apellido_materno='$ape_mat', email='$email', telefono='$tel' WHERE id='$id'";
 $query=mysqli_query($conn,$sql);
 
     if($query){
