0ae7133d7d0fc0236b38cac1df962e6ceda310e4
Chino94 <chinosalazar94@gmail.com>
2022-07-10 19:40:06 -0500

    Update delete_admin_sql.php

--- sql/delete_admin_sql.php
+++ sql/delete_admin_sql.php
@@ -4,8 +4,9 @@
 
 $id=$_GET['id'];
 
-$sql="DELETE FROM administradores WHERE id='$id'";
-$query=mysqli_query($conn,$sql);
+//$sql1="DELETE FROM administradores WHERE id='$id'";
+$sql2="DELETE FROM inicio WHERE id_admin='$id'";
+$query=mysqli_query($conn,$sql1,$sql2);
 
     if($query){
         Header("Location: ../vistas/admin_vista.php");
