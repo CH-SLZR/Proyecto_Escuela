d50f55f0259ad7335ff4a157de1c8959299e7e3d
Chino94 <chinosalazar94@gmail.com>
2022-07-19 20:29:25 -0500

    commit

---
+++ vistas/login_vista(antes).php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <title>UADY - Inicio de Sesion</title>
+  <link rel='stylesheet' href='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
+  <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+  <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+
+  <div class="login">
+        <div class="login-header">
+            <br>
+            <h4>Inicio de Sesion</h4>
+        </div>
+
+        <form action="../sql/login_sql.php" method="post">
+            <div class="login-form">
+
+                <h5>Usuario:</h5>
+                <input type="text" placeholder="Usuario" name="user" required /><br>
+                
+                <h5>Contraseña:</h5>
+                <input type="password" placeholder="Contraseña" name="contra" required /><br>
+
+                <!-- <select name="rol">
+                <option value="0" style="display:none;"><label>Seleccionar</label></option>
+                <option value="Paciente">Paciente</option>
+               <option value="Administrador">Administrador</option>
+                </select> -->
+
+                <button type="submit" class="boton_entrar" name="entrar">Entrar</button>
+            </div>
+        </form>
+  </div>
+</body>
+
+</html>
--- vistas/login_vista.php
+++ vistas/login_vista.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
-<html lang="es">
+﻿<!DOCTYPE html>
+<html lang="en">
 
 <head>
-  <title>UADY - Inicio de Sesion</title>
-  <link rel='stylesheet' href='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
-  <link rel="stylesheet" href="../style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
-  <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
-
-  <div class="login">
-        <div class="login-header">
-            <br>
-            <h4>Inicio de Sesion</h4>
+    <div class="con">
+        <div class="box">
+            <h1>Login Form</h1>
+            <form action="../sql/login_sql.php" method="post" class="login-form">
+                <input type="text" name="user" placeholder="Ingresa el usuario" required><br>
+                <input type="Password" name="contra" placeholder="Enter Password" required><br>
+                <!-- <input type="button" name="btn" value="Login " class="btn"> -->
+                <button type="submit" class="btn" name="entrar">Entrar</button>
+            </form>
         </div>
-
-        <form action="../sql/login_sql.php" method="post">
-            <div class="login-form">
-
-                <h5>Usuario:</h5>
-                <input type="text" placeholder="Usuario" name="user" required /><br>
-                
-                <h5>Contraseña:</h5>
-                <input type="password" placeholder="Contraseña" name="contra" required /><br>
-
-                <!-- <select name="rol">
-                <option value="0" style="display:none;"><label>Seleccionar</label></option>
-                <option value="Paciente">Paciente</option>
-               <option value="Administrador">Administrador</option>
-                </select> -->
-
-                <button type="submit" class="boton_entrar" name="entrar">Entrar</button>
-            </div>
-        </form>
-  </div>
+    </div>
 </body>
 
 </html>
