80e8c58773d0fe0aa4fcb7080febdf961a9862c0
Chino94 <chinosalazar94@gmail.com>
2022-07-26 14:43:33 -0500

    commit

--- vistas/admin_vista.php
+++ vistas/admin_vista.php
@@ -8,21 +8,19 @@
 
 <head>
     <title>UADY - Registro de Usuarios</title>
-    <link rel='stylesheet' href='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'>
-    <link rel="stylesheet" href="../style.css">
+    <!-- <link rel='stylesheet' href='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'> -->
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
-    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+    <div class="padre_admin">
+        <div= class="hijo_registro">
 
-    <div class="login">
-        <div class="login-header">
-            <h4>Registro de Usuarios</h4>
-        </div>
+            <!-- <div class="registro-header"> -->
+                <h2>Registro de Usuarios</h2>
+            <!-- </div> -->
 
-        <form action="../sql/registro_sql.php" method="post">
-            <div class="login-form">
-
+            <form action="../sql/registro_sql.php" method="post">
                 <h5>Nombre:</h5>
                 <input type="text" placeholder="Nombre(s)" name="name" required />
 
@@ -43,149 +41,158 @@
 
                 <h5>Contraseña:</h5>
                 <input type="text" placeholder="Contraseña" name="contra" required />
+                <br>
 
-                <select name="rol" required>
+                <select name="rol" class="btn" required>
                     <label>Seleccionar</label></option>
                     <option value="Paciente">Paciente</option>
                     <option value="Administrador">Administrador</option>
                 </select>
-                <br>
+                <br> <br>
 
-                <button type="submit" class="boton_entrar" name="registrar">Registrar</button>
-                <br>
-                <a href="../index.php"><button type="button">Cerrar sesion</button></a>
+                <button type="submit" class="btn" name="registrar">Registrar</button>
+                <br><br>
+                <a href="../index.php"><button class="btn" type="button">Cerrar sesion</button></a>
+            </form>
+        </div>
+
+        <div class="hijo_tablas">
+
+            <div class="tablas-header">
+                <h4>Tablas de Datos</h4>
             </div>
-        </form>
 
-        <div class="login-header">
-            <h4>Tablas de Datos</h4>
-        </div>
+            <!-- empieza tabla administradores -->
+            <div class="tabla_admin-header">
+                <h5>Tablas de Administradores</h5>
+            </div>
 
-        <!-- empieza tabla administradores -->
-        <div class="login-header">
-            <h5>Tablas de Administradores</h5>
-        </div>
+            <div class="tabla_admins">
+                <table class="table">
+                    <thead class="table-success table-striped">
+                        <tr>
+                            <th>id</th>
+                            <th>nombre</th>
+                            <th>apellido paterno</th>
+                            <th>apellido mateno</th>
+                            <th>email</th>
+                            <th>telefono</th>
+                        </tr>
+                    </thead>
 
-        <div class="tabla_admins">
-            <table class="table">
-                <thead class="table-success table-striped">
-                    <tr>
-                        <th>id</th>
-                        <th>nombre</th>
-                        <th>apellido paterno</th>
-                        <th>apellido mateno</th>
-                        <th>email</th>
-                        <th>telefono</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <?php
-                                         while($row=mysqli_fetch_array($query_admin)){
+                    <tbody>
+                        <?php
+                            while($row=mysqli_fetch_array($query_admin)){
                         ?>
-                    <tr>
-                        <th><?php  echo $row['id']?></th>
-                        <th><?php  echo $row['nombre']?></th>
-                        <th><?php  echo $row['apellido_paterno']?></th>
-                        <th><?php  echo $row['apellido_materno']?></th>
-                        <th><?php  echo $row['email']?></th>
-                        <th><?php  echo $row['telefono']?></th>
+                        <tr>
+                            <th><?php  echo $row['id']?></th>
+                            <th><?php  echo $row['nombre']?></th>
+                            <th><?php  echo $row['apellido_paterno']?></th>
+                            <th><?php  echo $row['apellido_materno']?></th>
+                            <th><?php  echo $row['email']?></th>
+                            <th><?php  echo $row['telefono']?></th>
 
-                        <th><a href="update_admin_vista.php?id=<?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
-                        <th><a href="../sql/delete_admin_sql.php?id=<?php echo $row['id'] ?>" class="btn_delete">Eliminar</a>
-                        </th>
-                    </tr>
-                    <?php 
-                                            }
-                                        ?>
-                </tbody>
-            </table>
-        </div>
-        <!-- termina tabla administradores -->
+                            <th><a href="update_admin_vista.php?id=<?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
 
-        <!-- empieza tabla pacientes -->
-        <div class="login-header">
-            <h5>Tablas de Pacientes</h5>
-        </div>
-
-        <div class="tabla_admins">
-            <table class="table">
-                <thead class="table-success table-striped">
-                    <tr>
-                        <th>id</th>
-                        <th>nombre</th>
-                        <th>apellido paterno</th>
-                        <th>apellido mateno</th>
-                        <th>email</th>
-                        <th>telefono</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <?php
-                                         while($row=mysqli_fetch_array($query_paci)){
+                            <th><a href="../sql/delete_admin_sql.php?id=<?php echo $row['id'] ?>"
+                                class="btn_delete">Eliminar</a>
+                            </th>
+                        </tr>
+                        <?php 
+                            }
                         ?>
-                    <tr>
-                        <th><?php  echo $row['id']?></th>
-                        <th><?php  echo $row['nombre']?></th>
-                        <th><?php  echo $row['apellido_paterno']?></th>
-                        <th><?php  echo $row['apellido_materno']?></th>
-                        <th><?php  echo $row['email']?></th>
-                        <th><?php  echo $row['telefono']?></th>
+                    </tbody>
+                </table>
+            </div>
+            <!-- termina tabla administradores -->
 
-                        <th><a href="update_paci_vista.php?id=<?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
-                        <th><a href="../sql/delete_paci_sql.php?id=<?php echo $row['id'] ?>" class="btn_delete">Eliminar</a>
-                        </th>
-                    </tr>
-                    <?php 
-                                            }
-                                        ?>
-                </tbody>
-            </table>
-        </div>
-        <!-- termina tabla pacientes -->
+            <!-- empieza tabla pacientes -->
+            <div class="login-header">
+                <h5>Tablas de Pacientes</h5>
+            </div>
 
-        <!-- empieza tabla inicio -->
-        <div class="login-header">
-            <h5>Tablas de Usuarios</h5>
-        </div>
+                <div class="tabla_admins">
+                    <table class="table">
+                        <thead class="table-success table-striped">
+                            <tr>
+                                <th>id</th>
+                                <th>nombre</th>
+                                <th>apellido paterno</th>
+                                <th>apellido mateno</th>
+                                <th>email</th>
+                                <th>telefono</th>
+                            </tr>
+                        </thead>
 
-        <div class="tabla_inicio">
-            <table class="table">
-                <thead class="table-success table-striped">
-                    <tr>
-                        <th>id</th>
-                        <th>usuario</th>
-                        <th>contraseña</th>
-                        <th>rol</th>
-                        <th>id_admin</th>
-                        <th>id_paciente</th>
-                    </tr>
-                </thead>
+                        <tbody>
+                            <?php
+                                while($row=mysqli_fetch_array($query_paci)){
+                            ?>
+                            <tr>
+                                <th><?php  echo $row['id']?></th>
+                                <th><?php  echo $row['nombre']?></th>
+                                <th><?php  echo $row['apellido_paterno']?></th>
+                                <th><?php  echo $row['apellido_materno']?></th>
+                                <th><?php  echo $row['email']?></th>
+                                <th><?php  echo $row['telefono']?></th>
 
-                <tbody>
-                    <?php
+                                <th><a href="update_paci_vista.php?id=<?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
+
+                                <th><a href="../sql/delete_paci_sql.php?id=<?php echo $row['id'] ?>"
+                                    class="btn_delete">Eliminar</a>
+                                </th>
+                            </tr>
+                            <?php 
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- termina tabla pacientes -->
+
+                <!-- empieza tabla inicio -->
+                <div class="login-header">
+                    <h5>Tablas de Usuarios</h5>
+                </div>
+
+            <div class="tabla_inicio">
+                <table class="table">
+                    <thead class="table-success table-striped">
+                        <tr>
+                            <th>id</th>
+                            <th>usuario</th>
+                            <th>contraseña</th>
+                            <th>rol</th>
+                            <th>id_admin</th>
+                            <th>id_paciente</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
                             while($row=mysqli_fetch_array($query_ini)){
                         ?>
-                    <tr>
-                        <th><?php  echo $row['id']?></th>
-                        <th><?php  echo $row['usuario']?></th>
-                        <th><?php  echo $row['contraseña']?></th>
-                        <th><?php  echo $row['rol']?></th>
-                        <th><?php  echo $row['id_admin']?></th>
-                        <th><?php  echo $row['id_paci']?></th>
+                        <tr>
+                            <th><?php  echo $row['id']?></th>
+                            <th><?php  echo $row['usuario']?></th>
+                            <th><?php  echo $row['contraseña']?></th>
+                            <th><?php  echo $row['rol']?></th>
+                            <th><?php  echo $row['id_admin']?></th>
+                            <th><?php  echo $row['id_paci']?></th>
 
-                        <!-- <th><a href="update_ini_vista.php?id=<//?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
-                        <th><a href="../sql/delete_ini_sql.php?id=<//?php echo $row['id'] ?>" class="btn_delete">Eliminar</a> -->
-                        </th>
-                    </tr>
-                    <?php 
-                                }
-                    ?>
-                </tbody>
-            </table>
+                            <!-- <th><a href="update_ini_vista.php?id=<//?php echo $row['id'] ?>" class="btn_edit">Editar</a></th>
+                            <th><a href="../sql/delete_ini_sql.php?id=<//?php echo $row['id'] ?>" class="btn_delete">Eliminar</a> -->
+                            </th>
+                        </tr>
+                        <?php 
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <!-- termina tabla inicio -->
         </div>
-        <!-- termina tabla inicio -->
     </div>
 </body>
+
 </html>
