cfb47f4aa8234f35a1fcc0bceea1efbdef813b95
Chino94 <chinosalazar94@gmail.com>
2022-07-26 20:39:05 -0500

    commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+
+<html>
+    <body>
+        <header>
+            <!-- aqui va el include -->
+        </header>
+    </body>
+</html>
+
 <?php
     header('Location: vistas/login_vista.php');
 ?>
