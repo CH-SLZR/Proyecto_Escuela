d3e03fac6468e201f766e829543876155454669d
Chino94 <chinosalazar94@gmail.com>
2022-07-28 03:27:33 -0500

    commit

--- vistas/update_paci_vista.php
+++ vistas/update_paci_vista.php
@@ -16,7 +16,6 @@
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
     <title>Actualizar Paciente</title>
     <link rel="stylesheet" href="../assets/css/style.css">
 
