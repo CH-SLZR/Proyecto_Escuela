34f88b5e210611316c6e7d47baadc0da79605d27
Chino94 <chinosalazar94@gmail.com>
2022-08-04 16:51:03 -0500

    COMMIT

--- sql/rutina_sql.php
+++ sql/rutina_sql.php
@@ -1,28 +1,25 @@
 <?php
-include('conexion.php');
-include('read.php');
+    $peso = $_POST["peso"];
+    $edad = $_POST["edad"];
+    $pre_art = $_POST["pre_art"];
+    $oxi = $_POST["oxi"];
+    $lat_min = $_POST["lat_min"];
+    $sexo = $_POST["sexo"];
 
-        $peso = $_POST["peso"];
-        $edad = $_POST["edad"];
-        $pre_art = $_POST["pre_art"];
-        $oxi = $_POST["oxi"];
-        $lat_min = $_POST["lat_min"];
-        $sexo = $_POST["sexo"];
+    if ($sexo == "Mujer"){
 
-        if ($sexo == "Mujer"){
+        $img = "../img/rutinas/rutina_1.jpg";
+        $dat = base64_encode(file_get_contents($img));
+        $src = 'data:'.mime_content_type($img).';base64,'.$dat;
 
-            $img = "../img/rutinas/rutina_1.jpg";
-            $dat = base64_encode(file_get_contents($img));
-            $src = 'data:'.mime_content_type($img).';base64,'.$dat;
+        echo "<img src=\"$src\">";
 
-            echo "<img src=\"$src\">";
+    }elseif ($sexo == "Hombre"){
 
-        }elseif ($sexo == "Hombre"){
+        $img = "../img/rutinas/rutina_5.jpg";
+        $dat = base64_encode(file_get_contents($img));
+        $src = 'data:'.mime_content_type($img).';base64,'.$dat;
 
-            $img = "../img/rutinas/rutina_5.jpg";
-            $dat = base64_encode(file_get_contents($img));
-            $src = 'data:'.mime_content_type($img).';base64,'.$dat;
-
-            echo "<img src=\"$src\">";
-        } 
+        echo "<img src=\"$src\">";
+    } 
 ?>
